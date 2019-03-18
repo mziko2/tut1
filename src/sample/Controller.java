385f783ac8aa88ddc2b9a0cb38fc38dca33a7b1b
FirstProject <dalila.herh@gmail.com>
2019-03-18 11:56:10 +0100

    css

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+
+    public void Say(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World");
+    }
 }
